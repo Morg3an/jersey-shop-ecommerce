7c0e1f1850ab10197f2abc9e55e01971a8712db3
Morg3an <mulweyemorgan12@gmail.com>
2025-07-19 02:15:32 +0300

    Production updates

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -1,6 +1,8 @@
-export const errorHandler = (err, req, res, next) => {
+const errorHandler = (err, req, res, next) => {
   console.error(err.stack);
   res.status(err.statusCode || 500).json({
     message: err.message || 'Something went wrong',
   });
 };
+
+module.exports = errorHandler;
